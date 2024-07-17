bd1ca9c8c38564811638ab2ac94a842cbcc64238
LacomWayne <lacomwayne@gmail.com>
2024-07-17 14:27:15 +0530

    Okay

--- src/app/api/form/route.js
+++ src/app/api/form/route.js
@@ -2,12 +2,13 @@ import clientPromise from '../mongodb';
 import { NextResponse } from 'next/server';
 export async function POST(request) {
     const { name, email, mobile, message } = await request.json();
+    var date = new Date();
+    date = date.toLocaleDateString('en-GB');
     try {
         const client = await clientPromise;
         const db = client.db('urbandetailing');
         const collection = db.collection('Queries');
-        await collection.insertOne({ name, email, mobile, message });
-        console.log("done");
+        await collection.insertOne({ name, email, mobile, message, date });
         return NextResponse.json({ msg: 'Query Submitted Successfully' });
     } catch (e) {
         console.error('Some Error Occurred\n' + e);
